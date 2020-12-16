e643c187f2fc6689e02efba949bfa4e720a13b56
Mirza Krak <mirza@maneoz.tech>
2020-12-16 16:22:03 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="mender-python-client-mendersoftware",
+    name="mender",
     version="0.0.1",
     license="Apache 2.0",
     author="Mendersoftware",
@@ -35,7 +35,7 @@ setuptools.setup(
     ],
     keywords=["mender", "OTA", "updater"],
     install_requires=["cryptography", "requests"],
-    entry_points={"console_scripts": ["mender-python-client-mendersoftware=mender:main"]},
+    entry_points={"console_scripts": ["mender=src:main"]},
     python_requires=">=3.6",
     zip_safe=False,
     include_package_data=True,
